6ab6a33ad21fd172af297d892e99c6ebe313fdfc
João Pedro <joaopedrodamaiamoura@outlook.com>
2023-10-23 12:17:19 -0300

    :tada: Atividade React 2 :tada:

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+import "./Home.css";
+
+function Home() {
+  return (
+    <main>
+      <h1>Bem vindos(as) a Home!</h1>
+      <h2>Hora de codar!</h2>
+    </main>
+  )
+}
+
+export default Home
